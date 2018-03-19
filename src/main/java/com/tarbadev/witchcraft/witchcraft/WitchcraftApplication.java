9dbf6e65fefbdb86c44ac5368e02891bc6c13908
Tony <tony@Tonys-Mac.local>
2018-03-18 22:10:10 -0700

    First commit

---
+++ src/main/java/com/tarbadev/witchcraft/witchcraft/WitchcraftApplication.java
@@ -0,0 +1,12 @@
+package com.tarbadev.witchcraft.witchcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitchcraftApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tarbadev/witchcraft/witchcraft/WitchcraftApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tarbadev.witchcraft.witchcraft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WitchcraftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
