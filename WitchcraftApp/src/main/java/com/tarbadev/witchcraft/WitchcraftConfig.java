d15f72de5af339b3ec5597fd6391695f8dfddd93
Tarbadev <tarbadev@gmail.com>
2018-03-26 20:43:51 -0700

    Added style to /recipes [finishes #156261624]

--- WitchcraftApp/src/main/java/com/tarbadev/witchcraft/WitchcraftConfig.java
+++ WitchcraftApp/src/main/java/com/tarbadev/witchcraft/WitchcraftConfig.java
@@ -12,18 +12,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ConfigurationProperties
 @EnableWebMvc
 public class WitchcraftConfig implements WebMvcConfigurer {
+    private static final String BACKEND = "/WitchcraftApp";
+    private static final String FRONTEND = "/WitchcraftUi";
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String workingDirectory = System.getProperty("user.dir");
 
-        if (workingDirectory.endsWith("/WitchcraftApp")) {
-            workingDirectory = workingDirectory.substring(0, workingDirectory.lastIndexOf("/backend"));
+        if (workingDirectory.endsWith(BACKEND)) {
+            workingDirectory = workingDirectory.substring(0, workingDirectory.lastIndexOf(BACKEND));
         }
 
         registry.addResourceHandler("/**").addResourceLocations(
                 "classpath:/static/",
                 "classpath:/",
-                "file:///" + workingDirectory + "/WitchcraftUi/build/dist/"
+                "file:///" + workingDirectory + FRONTEND + "/build/dist/"
         );
     }
 }
