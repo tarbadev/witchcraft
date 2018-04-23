214e9ebd51b401f06509c235fb3b0c8eeab7bd39
Tarbadev <tarbadev@gmail.com>
2018-04-22 22:25:24 -0700

    Recipes are visible in week view [finishes #156983379]

--- WitchcraftApp/src/main/java/com/tarbadev/witchcraft/web/WeekController.java
+++ WitchcraftApp/src/main/java/com/tarbadev/witchcraft/web/WeekController.java
@@ -1,6 +1,7 @@
 package com.tarbadev.witchcraft.web;
 
 import com.tarbadev.witchcraft.domain.GetCurrentWeekUseCase;
+import com.tarbadev.witchcraft.domain.RecipeCatalogUseCase;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WeekController {
   private GetCurrentWeekUseCase getCurrentWeekUseCase;
+  private RecipeCatalogUseCase recipeCatalogUseCase;
 
-  public WeekController(GetCurrentWeekUseCase getCurrentWeekUseCase) {
+  public WeekController(GetCurrentWeekUseCase getCurrentWeekUseCase, RecipeCatalogUseCase recipeCatalogUseCase) {
     this.getCurrentWeekUseCase = getCurrentWeekUseCase;
+    this.recipeCatalogUseCase = recipeCatalogUseCase;
   }
 
   @GetMapping("/weeks")
   public String index(Model model) {
     model.addAttribute("week", getCurrentWeekUseCase.execute());
+    model.addAttribute("recipes", recipeCatalogUseCase.execute());
     return "weeks/index";
   }
 }
--- WitchcraftApp/src/test/java/com/tarbadev/witchcraft/web/WeekControllerTest.java
+++ WitchcraftApp/src/test/java/com/tarbadev/witchcraft/web/WeekControllerTest.java
@@ -1,6 +1,8 @@
 package com.tarbadev.witchcraft.web;
 
 import com.tarbadev.witchcraft.domain.GetCurrentWeekUseCase;
+import com.tarbadev.witchcraft.domain.Recipe;
+import com.tarbadev.witchcraft.domain.RecipeCatalogUseCase;
 import com.tarbadev.witchcraft.domain.Week;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,24 +27,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class WeekControllerTest {
-  @Autowired
-  private MockMvc mvc;
+  @Autowired private MockMvc mvc;
   @Autowired private GetCurrentWeekUseCase getCurrentWeekUseCase;
+  @Autowired private RecipeCatalogUseCase recipesCatalogUseCase;
 
   @Before
   public void setUp() {
-    Mockito.reset(getCurrentWeekUseCase);
+    Mockito.reset(getCurrentWeekUseCase, recipesCatalogUseCase);
   }
 
   @Test
   public void index() throws Exception {
     Week week = Week.builder().build();
+    List<Recipe> recipes = Arrays.asList(
+        Recipe.builder().build(),
+        Recipe.builder().build(),
+        Recipe.builder().build(),
+        Recipe.builder().build(),
+        Recipe.builder().build()
+    );
 
     given(getCurrentWeekUseCase.execute()).willReturn(week);
+    given(recipesCatalogUseCase.execute()).willReturn(recipes);
 
     mvc.perform(get("/weeks"))
         .andExpect(status().isOk())
         .andExpect(view().name("weeks/index"))
-        .andExpect(model().attribute("week", week));
+        .andExpect(model().attribute("week", week))
+        .andExpect(model().attribute("recipes", recipes));
   }
 }
