9d5f674bce1d7e2e79db3f68fd5c97181ef42b04
Tarbadev <tarbadev@gmail.com>
2018-11-20 09:42:08 -0800

    Finalize adding react to project [finishes #157342918]

--- WitchcraftApp/src/main/java/com/tarbadev/witchcraft/WitchcraftApplication.java
+++ WitchcraftApp/src/main/java/com/tarbadev/witchcraft/WitchcraftApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-class WitchcraftApplication {
+public class WitchcraftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WitchcraftApplication.class, args);
