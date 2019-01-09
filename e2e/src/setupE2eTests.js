5d9cb811c674569e3d39ad35a9dc04aa1712abc1
Tarbadev <tarbadev@gmail.com>
2019-01-08 16:19:02 -0800

    Remove specific port for actuator [#162523692]

--- e2e/src/setupE2eTests.js
+++ e2e/src/setupE2eTests.js
@@ -7,14 +7,14 @@ export let page
 const width = 1920
 const height = 1080
 
-const backendUrl = process.env.BACKEND_URL ? process.env.BACKEND_URL : 'http://localhost'
-export const ActuatorUrl = `${backendUrl}:8888/actuator`
+const backendUrl = process.env.BACKEND_URL ? process.env.BACKEND_URL : 'http://localhost:8080'
+export const ActuatorUrl = `${backendUrl}/actuator`
 export const AppUrl = process.env.APP_URL ? process.env.APP_URL : 'http://localhost:5000'
 
 beforeAll(async (done) => {
   browser = await puppeteer.launch({
     headless: true,
-    args: [`--window-size=${width},${height}`],
+    args: [`--window-size=${width},${height}`, '-–no-sandbox', '-–disable-setuid-sandbox'],
   })
 
   page = await browser.newPage()
