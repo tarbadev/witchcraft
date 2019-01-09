b7cac82167e541f794389e737c9ef77def1b5c8e
Tarbadev <tarbadev@gmail.com>
2019-01-08 16:57:00 -0800

    Add history fallback functionality [#162523692]

--- Frontend/public/server.js
+++ Frontend/public/server.js
@@ -1,3 +1,4 @@
+const history = require('connect-history-api-fallback')
 const express = require('express')
 
 const app = express()
@@ -6,5 +7,6 @@ app.get('/api/*', (req, res) => {
   res.redirect(`${process.env.API_URL}${req.originalUrl}`)
 })
 
+app.use(history({ verbose: false }))
 app.use(express.static('.'))
 app.listen(8080, () => console.log('Listening on port 8080!'))
