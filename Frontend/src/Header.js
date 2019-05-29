88624b74561dcd07616496dafae0ba3ea33db9d8
Tarbadev <tarbadev@gmail.com>
2019-05-28 21:28:11 -0700

    [#166316804] Show which tab is selected

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -3,13 +3,53 @@ import { Link } from 'react-router-dom'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 
+import './Header.css'
+
 export const Header = () => {
+  const links = [
+    {
+      label: 'Home',
+      url: '/',
+    },
+    {
+      label: 'Recipes',
+      url: '/recipes',
+    },
+    {
+      label: 'Weeks',
+      url: '/weeks',
+    },
+    {
+      label: 'Carts',
+      url: '/carts',
+    },
+  ]
+
+  const currentUrl = window.location.pathname
+
+  const isCurrent = link => {
+    if (link.label === 'Home') {
+      return currentUrl === '' || currentUrl === '/'
+    }
+
+    return currentUrl.startsWith(link.url)
+  }
+
   return (
     <Toolbar disableGutters={true}>
-      <Button component={Link} to='/'>Home</Button>
-      <Button component={Link} to='/recipes'>Recipes</Button>
-      <Button component={Link} to='/weeks'>Weeks</Button>
-      <Button component={Link} to='/carts'>Carts</Button>
+      {
+        links.map((link, index) =>
+          <Button
+            key={`header-link-${index}`}
+            component={Link}
+            className={`header__link ${isCurrent(link) ? 'header__selected' : ''}`}
+            color={'default'}
+            href={link.url}
+            to={link.url}>
+            {link.label}
+          </Button>,
+        )
+      }
     </Toolbar>
   )
 }
