10417ec4eb3f81a2a61583e72882c547b0fd5d7b
Tarbadev <tarbadev@gmail.com>
2019-07-20 14:23:57 -0700

    [#167281168] Fix unit test

--- Frontend/src/weeks/actions/RecipeListModalActions.spec.js
+++ Frontend/src/weeks/actions/RecipeListModalActions.spec.js
@@ -5,17 +5,13 @@ describe('RecipeListModalActions', () => {
   describe('addExpressRecipe', () => {
     it('calls the api to add an express recipe', () => {
       const onSuccessSpy = jest.fn()
-      const day = 'Monday'
-      const meal = 'Lunch'
       const recipeName = 'Lasagna'
 
-      addExpressRecipe(recipeName, day, meal, onSuccessSpy)
-
-      expect(addExpressRecipe(recipeName, day, meal, onSuccessSpy)).toEqual(fetchAction({
+      expect(addExpressRecipe(recipeName, onSuccessSpy)).toEqual(fetchAction({
         url: '/api/recipes/express',
         method: 'POST',
         body: { name: recipeName },
-        onSuccess: expect.any(Function),
+        onSuccess: onSuccessSpy,
       }))
     })
   })
