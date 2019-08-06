79c266130b4265a848dd2dd0e015b2ba458998df
Tarbadev <tarbadev@gmail.com>
2019-08-06 11:33:30 -0700

    [#166316898] Add recipe image to week cell

--- Frontend/src/weeks/components/WeekRecipeCell.js
+++ Frontend/src/weeks/components/WeekRecipeCell.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import TableCell from '@material-ui/core/TableCell'
-import Typography from '@material-ui/core/Typography'
 
 import './WeekRecipeCell.css'
-
-const RecipePaper = withStyles({
-  root: {
-    'padding': '5px',
-  },
-})(Paper)
+import { RecipeCard } from 'src/recipes/components/RecipeCard'
 
 export const WeekRecipeCell = ({ recipe, onCellClick, dataTag }) => {
   const classes = onCellClick
@@ -25,9 +17,7 @@ export const WeekRecipeCell = ({ recipe, onCellClick, dataTag }) => {
     >
       {onCellClick && <div className='hover-mask' />}
       {(recipe && recipe.name) &&
-      <RecipePaper>
-        <Typography>{recipe.name}</Typography>
-      </RecipePaper>
+      <RecipeCard title={recipe.name} imgUrl={recipe.imgUrl} />
       }
     </TableCell>
   )
