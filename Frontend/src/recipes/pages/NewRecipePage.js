26c3336b6b3434f8f53273f6e8e58ff05f6a2dea
Tarbadev <tarbadev@gmail.com>
2020-01-26 16:13:23 -0800

    [#167959557] Make new recipe page responsive

--- Frontend/src/recipes/pages/NewRecipePage.js
+++ Frontend/src/recipes/pages/NewRecipePage.js
@@ -25,7 +25,7 @@ export const NewRecipePageContainer = ({
 }) => {
   const { state, dispatch, setHeaderConfig } = useAppContext()
 
-  useEffect(() => setHeaderConfig({ currentLink: RECIPE, title: RECIPE, ...initialState.headerConfig }), [])
+  useEffect(() => setHeaderConfig({ ...initialState.headerConfig, currentLink: RECIPE, title: RECIPE }), [])
 
   const useSupportedDomains = () => {
     const [supportedDomains, setSupportedDomains] = useState(state.pages.newRecipePage.supportedDomains)
@@ -86,118 +86,114 @@ export const NewRecipePage = ({
     <Grid container spacing={3} justify='flex-start' alignItems='flex-start'>
       <PageTitle title='New Recipe' />
       <Grid item xs={12}>
-        <Typography variant='h4'>New Recipe</Typography>
+        <Typography variant='h4' className='witchcraft-title'>New Recipe</Typography>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={6}>
         <Paper className={classes.paper}>
-          <form>
-            <Grid container spacing={3} justify='flex-start' alignItems='flex-start'>
-              <Grid item xs={12}>
-                <Typography variant='h6'>Add manually</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container direction='row'>
-                  <Grid item xs={9}>
-                    <TextField
-                      label='Name'
-                      className='manual__name'
-                      fullWidth
-                      onChange={(e) => setManualFormName(e.target.value)}
-                      value={manualFormName}
-                      placeholder='Mini Goat Cheese Stuffed Potato Appetizers'
-                      type='text' />
-                  </Grid>
-                  <Grid item xs={3}>
-                    <TextField
-                      label='Portions'
-                      className='manual__portions'
-                      fullWidth
-                      onChange={(e) => setManualFormPortions(e.target.value)}
-                      value={manualFormPortions}
-                      placeholder='4'
-                      type='text' />
-                  </Grid>
-                </Grid>
-                <TextField
-                  label='Url'
-                  className='manual__url'
-                  fullWidth
-                  onChange={(e) => setManualFormUrl(e.target.value)}
-                  value={manualFormUrl}
-                  placeholder='http://example.com/recipe/32434'
-                  type='text' />
-                <TextField
-                  label='Image Url'
-                  className='manual__image-url'
-                  fullWidth
-                  onChange={(e) => setManualFormImageUrl(e.target.value)}
-                  value={manualFormImageUrl}
-                  placeholder='http://example.com/recipe/32434.png'
-                  type='text' />
-                <TextField
-                  label='Ingredients'
-                  className='manual__ingredients'
-                  name='ingredients'
-                  multiline
-                  fullWidth
-                  onChange={(e) => setManualFormIngredients(e.target.value)}
-                  value={manualFormIngredients}
-                  placeholder='2 oz. soft goat cheese'
-                  type='text' />
-                <TextField
-                  label='Steps'
-                  className='manual__steps'
-                  name='steps'
-                  multiline
-                  fullWidth
-                  onChange={(e) => setManualFormSteps(e.target.value)}
-                  value={manualFormSteps}
-                  placeholder='Add ingredients and stir\nServe on each plate'
-                  type='text' />
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  variant='contained'
-                  className='manual__submit-button'
-                  color='primary'
-                  href=''
-                  onClick={onManualUrlFormSubmit}>
-                  Submit
-                </Button>
-              </Grid>
+          <Grid container spacing={3} justify='flex-start' alignItems='flex-start'>
+            <Grid item xs={12}>
+              <Typography variant='h6' className='witchcraft-title'>Add manually</Typography>
             </Grid>
-          </form>
+            <Grid item xs={12}>
+              <Grid container direction='row'>
+                <Grid item xs={9}>
+                  <TextField
+                    label='Name'
+                    className='manual__name'
+                    fullWidth
+                    onChange={(e) => setManualFormName(e.target.value)}
+                    value={manualFormName}
+                    placeholder='Mini Goat Cheese Stuffed Potato Appetizers'
+                    type='text' />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    label='Portions'
+                    className='manual__portions'
+                    fullWidth
+                    onChange={(e) => setManualFormPortions(e.target.value)}
+                    value={manualFormPortions}
+                    placeholder='4'
+                    type='text' />
+                </Grid>
+              </Grid>
+              <TextField
+                label='Url'
+                className='manual__url'
+                fullWidth
+                onChange={(e) => setManualFormUrl(e.target.value)}
+                value={manualFormUrl}
+                placeholder='http://example.com/recipe/32434'
+                type='text' />
+              <TextField
+                label='Image Url'
+                className='manual__image-url'
+                fullWidth
+                onChange={(e) => setManualFormImageUrl(e.target.value)}
+                value={manualFormImageUrl}
+                placeholder='http://example.com/recipe/32434.png'
+                type='text' />
+              <TextField
+                label='Ingredients'
+                className='manual__ingredients'
+                name='ingredients'
+                multiline
+                fullWidth
+                onChange={(e) => setManualFormIngredients(e.target.value)}
+                value={manualFormIngredients}
+                placeholder='2 oz. soft goat cheese'
+                type='text' />
+              <TextField
+                label='Steps'
+                className='manual__steps'
+                name='steps'
+                multiline
+                fullWidth
+                onChange={(e) => setManualFormSteps(e.target.value)}
+                value={manualFormSteps}
+                placeholder='Add ingredients and stir\nServe on each plate'
+                type='text' />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant='contained'
+                className='manual__submit-button'
+                color='primary'
+                href=''
+                onClick={onManualUrlFormSubmit}>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
         </Paper>
       </Grid>
-      <Grid item container spacing={1} xs={6} justify='flex-start' alignItems='flex-start'>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <Grid container spacing={3} justify='flex-start' alignItems='flex-start'>
-              <Grid item xs={12}>
-                <Typography variant='h6'>Add from URL</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  className='auto__url'
-                  onChange={(e) => setUrlFormValue(e.target.value)}
-                  value={urlFormValue}
-                  label='http://example.com/recipe/32434'
-                  type='search' />
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  variant='contained'
-                  className='auto__submit-button'
-                  color='primary'
-                  href=''
-                  onClick={onUrlFormSubmit}>
-                  Submit
-                </Button>
-              </Grid>
+      <Grid item xs={12} sm={6}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={3} justify='flex-start' alignItems='flex-start'>
+            <Grid item xs={12}>
+              <Typography variant='h6' className='witchcraft-title'>Add from URL</Typography>
             </Grid>
-          </Paper>
-        </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                className='auto__url'
+                onChange={(e) => setUrlFormValue(e.target.value)}
+                value={urlFormValue}
+                label='http://example.com/recipe/32434'
+                type='search' />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant='contained'
+                className='auto__submit-button'
+                color='primary'
+                href=''
+                onClick={onUrlFormSubmit}>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
         <GridList cellHeight={50} cols={2} className='supported-domains'>
           {supportedDomainList}
         </GridList>
